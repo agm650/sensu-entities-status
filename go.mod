bd908cc73ba7a91ffa4022d0463703309d514221
Luc Dandoy <luc.dandoy@gmail.com>
2020-05-31 14:19:58 +0200

    First set of test

--- go.mod
+++ go.mod
@@ -1,9 +1,10 @@
-module "las/accs/entities-status"
+module las/accs/entities-status
 
 go 1.14
 
 require (
+	github.com/apex/log v1.3.0
 	github.com/sensu-community/sensu-plugin-sdk v0.6.2
 	github.com/sensu/sensu-go v0.0.0-20200131164840-40b1d5938251
-	github.com/apex/log v1.3.0
+	github.com/stretchr/testify v1.6.0
 )
---
+++ sensu/entities_test.go
@@ -0,0 +1,55 @@
+package sensu
+
+import (
+	"testing"
+
+	"github.com/sensu-community/sensu-plugin-sdk/sensu"
+	"github.com/stretchr/testify/assert"
+)
+
+// GetEntitiesFromEvents : Get a list of entities based on a list of event
+func TestGetEntitiesFromEvents(t *testing.T) {
+	// assert := assert.New(t)
+}
+
+func TestCalculateStatus(t *testing.T) {
+	assert := assert.New(t)
+
+	// New and old event are identical, status should be equal to input
+	assert.Equal(calculateStatus(sensu.CheckStateOK, sensu.CheckStateOK), sensu.CheckStateOK)
+	assert.Equal(calculateStatus(sensu.CheckStateCritical, sensu.CheckStateCritical), sensu.CheckStateCritical)
+	assert.Equal(calculateStatus(sensu.CheckStateWarning, sensu.CheckStateWarning), sensu.CheckStateWarning)
+	assert.Equal(calculateStatus(sensu.CheckStateUnknown, sensu.CheckStateUnknown), sensu.CheckStateUnknown)
+
+	// Old state is critical. No matter what it has to remain critical
+	assert.Equal(calculateStatus(sensu.CheckStateCritical, sensu.CheckStateUnknown), sensu.CheckStateCritical)
+	assert.Equal(calculateStatus(sensu.CheckStateCritical, sensu.CheckStateOK), sensu.CheckStateCritical)
+	assert.Equal(calculateStatus(sensu.CheckStateCritical, sensu.CheckStateWarning), sensu.CheckStateCritical)
+
+	// New state is critical. No matter what it has to change to critical
+	assert.Equal(calculateStatus(sensu.CheckStateUnknown, sensu.CheckStateCritical), sensu.CheckStateCritical)
+	assert.Equal(calculateStatus(sensu.CheckStateOK, sensu.CheckStateCritical), sensu.CheckStateCritical)
+	assert.Equal(calculateStatus(sensu.CheckStateWarning, sensu.CheckStateCritical), sensu.CheckStateCritical)
+
+	// Old state is warning. It can only change if new state is Critical
+	assert.Equal(calculateStatus(sensu.CheckStateWarning, sensu.CheckStateUnknown), sensu.CheckStateWarning)
+	assert.Equal(calculateStatus(sensu.CheckStateWarning, sensu.CheckStateOK), sensu.CheckStateWarning)
+	assert.Equal(calculateStatus(sensu.CheckStateWarning, sensu.CheckStateWarning), sensu.CheckStateWarning)
+	assert.Equal(calculateStatus(sensu.CheckStateWarning, sensu.CheckStateCritical), sensu.CheckStateCritical)
+
+	// New state is warning. It will change result unless old state is Critical
+	assert.Equal(calculateStatus(sensu.CheckStateUnknown, sensu.CheckStateWarning), sensu.CheckStateWarning)
+	assert.Equal(calculateStatus(sensu.CheckStateOK, sensu.CheckStateWarning), sensu.CheckStateWarning)
+	assert.Equal(calculateStatus(sensu.CheckStateWarning, sensu.CheckStateWarning), sensu.CheckStateWarning)
+	assert.Equal(calculateStatus(sensu.CheckStateCritical, sensu.CheckStateWarning), sensu.CheckStateCritical)
+}
+
+// GetEntityStatus : Get an entity status based on a list of events
+func TestGetEntityStatus(t *testing.T) {
+	// assert := assert.New(t)
+}
+
+// GetEntitiesStatus : Get entities status based on a list of event
+func TestGetEntitiesStatus(t *testing.T) {
+	// assert := assert.New(t)
+}
